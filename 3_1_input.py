4ca29ed66b8621ae9131f61215c7b2bc828b202c
fariil <34712311+fariil@users.noreply.github.com>
2018-03-05 23:35:07 +0330

    Update 3_1_input.py
    get users age

--- 3_1_input.py
+++ 3_1_input.py
@@ -3,9 +3,12 @@
 ##########################
 text = input("enter what you want\n")
 print(5*text)
-
+text = input ("enter your alphabet age\n")
+print("you are" +text+ "years old" )
 ##########################
 # get a number from user #
 ##########################
 number=int(input("enter an integer\n"))
 print(5*number)
+text = input ("enter your numeric age\n")
+print("you are" +text+ "years old" )

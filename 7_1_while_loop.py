92d649253a3d5fe9e66937346276d50f11a9fb73
fariil <34712311+fariil@users.noreply.github.com>
2018-03-05 23:52:02 +0330

    Update 7_1_while_loop.py
    register age

--- 7_1_while_loop.py
+++ 7_1_while_loop.py
@@ -6,3 +6,9 @@ while a != "Ali":
     print("oops you are not a legal user")
     a = input("please enter your name\n")
 print("thanks for your registration")
+##my practice
+ b= input("please enter your age\n")
+    while b >= "100":
+         print("oops youre age is not a legal ")
+    b = input("please enter your age\n")
+print("thanks for your registration")

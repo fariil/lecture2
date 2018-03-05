e2a97213defe83895b65cf45d5e67d6264a2baca
fariil <34712311+fariil@users.noreply.github.com>
2018-03-05 23:27:11 +0330

    Update 4_1_comparison_operator.py
    new var

--- 4_1_comparison_operator.py
+++ 4_1_comparison_operator.py
@@ -9,3 +9,12 @@ print("i <= j --> {0}".format(i<=j))
 print("i >= j --> {0}".format(i>=j))
 print("i is equal to j --> {0}".format(i==j))
 print("i is not equal to j --> {0}".format(i!=j))
+#new var
+x=5
+y=6
+print("i < j --> {0}".format(x<y))
+print("i > j --> {0}".format(x>y))
+print("i <= j --> {0}".format(x<=y))
+print("i >= j --> {0}".format(x>=y))
+print("i is equal to j --> {0}".format(x==y))
+print("i is not equal to j --> {0}".format(x!=y))

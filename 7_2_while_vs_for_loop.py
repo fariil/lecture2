3de40f98dbd179e08d00e8f8ddabdabf51b4d6d3
fariil <34712311+fariil@users.noreply.github.com>
2018-03-06 00:05:01 +0330

    Update 7_2_while_vs_for_loop.py
    practice floor

--- 7_2_while_vs_for_loop.py
+++ 7_2_while_vs_for_loop.py
@@ -9,3 +9,10 @@ print("inside for loop")
 year=int(input("please enter year\n"))
 for x in range(temp_year,2029,2):
     print(x)
+    
+  ##my practice  
+    num = int(input("pls enter your class number\n"))
+    temp_num = num
+    while num <111
+    print("your class is on first floor")
+   
